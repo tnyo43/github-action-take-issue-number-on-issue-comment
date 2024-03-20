f774f62c542d4e233bc4623408751f717d616fef
Tomoya Kashifuku <tnyo43@gmail.com>
2024-03-20 11:56:19 +0900

    implement

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import * as core from "@actions/core";
+import { context } from "@actions/github";
+
+/**
+ * The main function for the action.
+ * @returns {Promise<void>} Resolves when the action is complete.
+ */
+export async function run() {
+  try {
+    console.log(context.payload);
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    if (error instanceof Error) core.setFailed(error.message);
+  }
+}
